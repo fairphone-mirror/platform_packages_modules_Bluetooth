eae07dc4c7a9221ba4d93827e71adc26606f7595
Bharath <bharath@teamb58.org>
2026-04-16 21:38:25 +0530

    Revert "Scan Optimization using iotconfigfeature for specific scan channel"
    This reverts commit ac7319feb8e0c6879f7a85abf9d742c3fb1fdba4.
    
    This is relevant only for IoT devices which also has dependency on
    additional project that does not exist in current tree. Hence, drop
    this change.
    
    Issue: FP4-A15#509
    Change-Id: Ide6e24ebb023c76bdb489f4a99759bd6a12aed5b

--- android/app/src/com/android/bluetooth/le_scan/ScanClient.java
+++ android/app/src/com/android/bluetooth/le_scan/ScanClient.java
@@ -12,10 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Changes from Qualcomm Technologies, Inc. are provided under the following license:
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.bluetooth.le_scan;
@@ -24,7 +20,6 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanSettings;
 import android.os.Binder;
 import android.os.UserHandle;
-import com.android.qcomfeatureconfig.QcomScanChannelConfig;
 
 import java.util.List;
 import java.util.Objects;
@@ -113,20 +108,16 @@ public class ScanClient {
         }
 
         ScanSettings.Builder builder = new ScanSettings.Builder();
-        builder.setScanMode(newScanMode)
-            .setCallbackType(settings.getCallbackType())
-            .setScanResultType(settings.getScanResultType())
-            .setReportDelay(settings.getReportDelayMillis())
-            .setNumOfMatches(settings.getNumOfMatches())
-            .setMatchMode(settings.getMatchMode())
-            .setLegacy(settings.getLegacy())
-            .setPhy(settings.getPhy());
-
-        if (QcomScanChannelConfig.TARGET_QCOM_IOT_SCANCHANNEL) {
-            builder.setScanChannel(settings.getScanChannel());
-        }
-
-        settings = builder.build();
+        settings =
+                builder.setScanMode(newScanMode)
+                        .setCallbackType(settings.getCallbackType())
+                        .setScanResultType(settings.getScanResultType())
+                        .setReportDelay(settings.getReportDelayMillis())
+                        .setNumOfMatches(settings.getNumOfMatches())
+                        .setMatchMode(settings.getMatchMode())
+                        .setLegacy(settings.getLegacy())
+                        .setPhy(settings.getPhy())
+                        .build();
         return true;
     }
 }
